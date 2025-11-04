40c087779c4a1957314889513035fe5702bdcd58
gdbs1107 <gdbs1107@naver.com>
2025-11-04 13:18:13 +0900

    init: 프로젝트 초기 설정

---
+++ src/test/java/or/hyu/ssd/SsdApplicationTests.java
@@ -0,0 +1,13 @@
+package or.hyu.ssd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
