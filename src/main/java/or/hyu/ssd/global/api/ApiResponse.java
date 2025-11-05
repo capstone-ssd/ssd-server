a5c2cd2fd3f6bfaa0f974c237d650651cf383980
gdbs1107 <gdbs1107@naver.com>
2025-11-06 00:33:12 +0900

    chore:#1 불필요한 응답 메서드 삭제

--- src/main/java/or/hyu/ssd/global/api/ApiResponse.java
+++ src/main/java/or/hyu/ssd/global/api/ApiResponse.java
@@ -10,22 +10,6 @@ public record ApiResponse<T>(String code, String msg, T data) {
         return new ApiResponse<T>(String.valueOf(HttpStatus.OK.value()), msg, data);
     }
 
-    public static <T> ApiResponse<T> created(T data) {
-        return new ApiResponse<T>(String.valueOf(HttpStatus.CREATED.value()), "응답 성공", data);
-    }
-
-    public static <T> ApiResponse<T> ok(T data) {
-        return ok(data, "응답 성공");
-    }
-
-    public static <T> ApiResponse<T> fail(String code, String msg) {
-        return new ApiResponse<T>(code, msg, null);
-    }
-
-    public static <T> ApiResponse<T> fail(String code,T data, String msg) {
-        return new ApiResponse<T>(code,msg,data);
-    }
-
     public static <T> ApiResponse<T> fail(ErrorCode errorCode) {
         return new ApiResponse<T>(errorCode.getCode(), errorCode.getMessage(), null);
     }
