8eb7f3da16e1a630e8278f754bef523b70288c6e
gdbs1107 <gdbs1107@naver.com>
2025-11-06 00:35:45 +0900

    doc:#1 ErrorCode 컨벤션 주석 처리

--- src/main/java/or/hyu/ssd/global/api/ErrorCode.java
+++ src/main/java/or/hyu/ssd/global/api/ErrorCode.java
@@ -8,7 +8,21 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ErrorCode {
 
-    SERVER_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR,"SERVER5001","서버에서 예외가 발생하였습니다. 개발자에게 문의해주세요");
+
+    /**
+     * code는 "도메인+HTTP STATUS+인덱스" 를 통해서 설정됩니다
+     * 도메인을 이용하여 에러코드를 분리해주세요
+     * */
+
+    // 서버 예외
+    SERVER_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR,"SERVER50001","서버에서 예외가 발생하였습니다. 개발자에게 문의해주세요");
+
+
+    // 회원 예외
+
+
+    // 문서 예외
+
 
     private final HttpStatus status;
     private final String code;
