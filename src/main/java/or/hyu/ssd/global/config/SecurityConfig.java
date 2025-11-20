6a84e102808e1d09e715b154905973e691314005
gdbs1107 <gdbs1107@naver.com>
2025-11-20 17:02:56 +0900

    feat:#11 security config 구현

---
+++ src/main/java/or/hyu/ssd/global/config/SecurityConfig.java
@@ -0,0 +1,104 @@
+package or.hyu.ssd.global.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+
+        return configuration.getAuthenticationManager();
+    }
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+        http
+                .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+                        CorsConfiguration configuration = new CorsConfiguration();
+
+                        /**
+                         * CORS 정책 대상 URL을 리스트로 관리
+                         * 다양한 메서드에 대해 CORS 정책을 허용합니다
+                         * */
+                        configuration.setAllowedOrigins(Arrays.asList(
+                                "http://localhost:5173",
+                                "http://localhost:5174"
+                        ));
+                        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+                        configuration.setAllowCredentials(true);
+                        configuration.setAllowedHeaders(Collections.singletonList("*"));
+                        configuration.setMaxAge(3600L);
+
+                        // exposedHeaders에 중복 설정 제거하고, 두 개의 헤더를 노출
+                        configuration.setExposedHeaders(Arrays.asList(
+                                "Set-Cookie",
+                                "access",
+                                "Authorization",
+                                "access-token"
+                        ));
+
+
+                        return configuration;
+                    }
+                }));
+
+
+
+
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        http.formLogin(AbstractHttpConfigurer::disable);
+        http.httpBasic(AbstractHttpConfigurer::disable);
+
+
+        // 인가 경로 설정
+        http.authorizeHttpRequests((auth)->auth
+                .requestMatchers(WhiteListConfig.swaggerWhitelist().toArray(new String[0])).permitAll()
+                .requestMatchers(WhiteListConfig.oauthWhitelist().toArray(new String[0])).permitAll()
+                .anyRequest().authenticated());
+
+
+        http
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http.logout(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
+}
+
---
+++ src/main/java/or/hyu/ssd/global/config/WhiteListConfig.java
@@ -0,0 +1,29 @@
+package or.hyu.ssd.global.config;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class WhiteListConfig {
+
+    // 스웨거 관련 인가 설정
+    public static final List<String> swaggerWhitelist() {
+        return List.of(
+                "/v3/api-docs/**",
+                "/swagger-ui/**",
+                "/swagger-ui.html"
+        );
+    }
+
+    // oauth 관련 인가 설정
+    public static final List<String> oauthWhitelist() {
+        return List.of(
+                "/oauth/kakao/",
+                "/oauth/kakao/callback",
+                "/access",
+                "/reissue"
+        );
+    }
+}
