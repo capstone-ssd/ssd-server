814946e1847e641b8eea752a7d2feb154d95e2d7
gdbs1107 <gdbs1107@naver.com>
2025-11-20 16:16:51 +0900

    feat:#3 체크리스트 엔티티 생성

---
+++ src/main/java/or/hyu/ssd/domain/document/entity/CheckList.java
@@ -0,0 +1,27 @@
+package or.hyu.ssd.domain.document.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import or.hyu.ssd.global.entity.BaseEntity;
+import org.hibernate.annotations.Comment;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Builder
+@Table(name = "check_lists")
+public class CheckList extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Comment("체크리스트 본문")
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Comment("체크리스트 체크 여부")
+    @Column(name = "checked", nullable = false)
+    private Boolean checked;
+}
