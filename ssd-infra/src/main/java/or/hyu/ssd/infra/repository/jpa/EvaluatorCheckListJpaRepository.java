4c9b84f0c23cd2307076912d0461d00f2a89e572
gdbs1107 <gdbs1107@naver.com>
2026-02-02 21:45:43 +0900

    feat:#57 인프라 JPA 레포지토리 추가

---
+++ ssd-infra/src/main/java/or/hyu/ssd/infra/repository/jpa/EvaluatorCheckListJpaRepository.java
@@ -0,0 +1,13 @@
+package or.hyu.ssd.infra.repository.jpa;
+
+import or.hyu.ssd.domain.document.entity.Document;
+import or.hyu.ssd.domain.document.entity.EvaluatorCheckList;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EvaluatorCheckListJpaRepository extends JpaRepository<EvaluatorCheckList, Long> {
+    List<EvaluatorCheckList> findAllByDocumentOrderByIdAsc(Document document);
+
+    void deleteAllByDocument(Document document);
+}
