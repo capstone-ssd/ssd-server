27996a09be8451bc68568149e905842c89554347
gdbs1107 <gdbs1107@naver.com>
2026-03-23 13:54:12 +0900

    fix: dev api 도메인 CORS 허용 추가

--- ssd-api/src/main/java/or/hyu/ssd/global/config/SecurityConfig.java
+++ ssd-api/src/main/java/or/hyu/ssd/global/config/SecurityConfig.java
@@ -64,7 +64,8 @@ public class SecurityConfig {
                                 "http://localhost:5173",
                                 "http://localhost:5174",
                                 "http://localhost:1573",
-                                "https://domain.com"
+                                "https://domain.com",
+                                "https://dev-api.simsaimdang.shop"
                         ));
                         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
                         configuration.setAllowCredentials(true);
