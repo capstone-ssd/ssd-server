c57d0e9d339463d22c9b7fdb639f8044b2548764
gdbs1107 <gdbs1107@naver.com>
2026-03-31 10:16:05 +0900

    feat:#126 S3 연동 기반 설정 추가

---
+++ ssd-core/src/main/java/or/hyu/ssd/global/config/properties/S3Properties.java
@@ -0,0 +1,27 @@
+package or.hyu.ssd.global.config.properties;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties(prefix = "app.storage.s3")
+public class S3Properties {
+
+    private boolean enabled = false;
+
+    private String bucket;
+
+    private String region;
+
+    private String accessKey;
+
+    private String secretKey;
+
+    private String endpoint;
+
+    private String publicBaseUrl;
+}
