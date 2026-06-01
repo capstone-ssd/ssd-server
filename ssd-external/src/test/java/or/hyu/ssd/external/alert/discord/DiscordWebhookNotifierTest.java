b753cd7e0b023bab22f3d40aea744ac558bd7b9d
gdbs1107 <gdbs1107@naver.com>
2026-06-01 11:51:45 +0900

    fix:#198 테스트 컨벤션 주석 보강

--- ssd-external/src/test/java/or/hyu/ssd/external/alert/discord/DiscordWebhookNotifierTest.java
+++ ssd-external/src/test/java/or/hyu/ssd/external/alert/discord/DiscordWebhookNotifierTest.java
@@ -14,13 +14,16 @@ class DiscordWebhookNotifierTest {
     @Test
     @DisplayName("디스코드 본문이 2000자를 넘으면 잘라낸다")
     void truncateMessageWhenContentTooLong() throws Exception {
+        // given
         DiscordWebhookNotifier notifier = new DiscordWebhookNotifier(new DiscordProperties(), new MockEnvironment());
         Method truncateMethod = DiscordWebhookNotifier.class.getDeclaredMethod("truncateForDiscord", String.class);
         truncateMethod.setAccessible(true);
-
         String longMessage = "a".repeat(2100);
+
+        // when
         String truncated = (String) truncateMethod.invoke(notifier, longMessage);
 
+        // then
         assertThat(truncated.length()).isLessThanOrEqualTo(2000);
         assertThat(truncated).endsWith("\n...(truncated)");
     }
@@ -28,13 +31,16 @@ class DiscordWebhookNotifierTest {
     @Test
     @DisplayName("디스코드 본문이 2000자 이하면 그대로 둔다")
     void keepMessageWhenWithinLimit() throws Exception {
+        // given
         DiscordWebhookNotifier notifier = new DiscordWebhookNotifier(new DiscordProperties(), new MockEnvironment());
         Method truncateMethod = DiscordWebhookNotifier.class.getDeclaredMethod("truncateForDiscord", String.class);
         truncateMethod.setAccessible(true);
-
         String message = "a".repeat(2000);
+
+        // when
         String truncated = (String) truncateMethod.invoke(notifier, message);
 
+        // then
         assertThat(truncated).isEqualTo(message);
     }
 }
